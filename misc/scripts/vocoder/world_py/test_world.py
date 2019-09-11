dfe8188b2e6397d2ba2210a1d9385098052ae948
GUANGYAN ZHANG <guangyanee@gmail.com>
2019-09-11 14:39:55 +0800

    reinitialize version

--- misc/scripts/vocoder/world_py/test_world.py
+++ misc/scripts/vocoder/world_py/test_world.py
@@ -8,7 +8,10 @@ import multiprocessing as mp
 import numpy as np
 import pyworld
 import pysptk
+from scipy.io import wavfile
 import librosa
+import scipy
+import pdb
 # initializations
 fs = 16000
 if fs == 16000:
@@ -75,31 +78,68 @@ def process(filename):
     :return: .lf0, .mgc and .bap files
     '''
 
+    # pdb.set_trace()
     file_id = os.path.basename(filename).split(".")[0]
     print('\n' + file_id)
 
     ### WORLD ANALYSIS -- extract vocoder parameters ###
-    ### extract sp, ap ###
-    x, fs = librosa.core.load(filename, sr=16000)
-    x = x.astype(np.float64)
-    f0, timeaxis = pyworld.harvest(x, fs, frame_period=5, f0_floor=71.0, f0_ceil=700)
+    # x, fs = librosa.core.load(filename, sr=16000)
+    fs, x = wavfile.read(filename)
+    alpha = pysptk.util.mcepalpha(fs)
+    print(alpha)
+    hopesize=int(0.005*fs)
+    # pdb.set_trace()
+    f0 = pysptk.rapt(x.astype(np.float32), fs=fs, hopsize=hopesize, min=60, max=600, voice_bias=0.0, otype=1)
+    f0=f0.astype(np.float64)
+    x = x.astype(np.float64)/(2**15)
+    _, timeaxis = pyworld.harvest(x, fs, frame_period=5, f0_floor=60.0, f0_ceil=600)
+    pdb.set_trace()
     spectrogram = pyworld.cheaptrick(x, f0, timeaxis, fs)
     aperiodicity = pyworld.d4c(x, f0, timeaxis, fs)
+    # pdb.set_trace()
     f0 = f0[:, None]
     lf0 = f0.copy()
-    nonzero_indices = np.nonzero(f0)
+    lf0 = lf0.astype(np.float32)
+    nonzero_indices = np.where(f0 != 0)
     lf0[nonzero_indices] = np.log(f0[nonzero_indices])
+    zero_indices = np.where(f0 == 0)
+    lf0[zero_indices] = -1.0E+10
+    write_binfile(lf0, os.path.join('./', file_id + '.lf0'),dtype=np.float32)
     mc = pysptk.sp2mc(spectrogram, mcsize, alpha=alpha)
+    mc = mc.astype(np.float32)
+    write_binfile(mc, os.path.join('./', file_id + '.mgc'),dtype=np.float32)
     bap = pyworld.code_aperiodicity(aperiodicity, fs)
-    bin_lf0 = read_binfile("1.lf0",dim=1)
-    bin_bap = read_binfile("1.bap",dim=1)
-    bin_mc = read_binfile("1.mgc", dim=60)
-    bin_lf0_2,_ = load_binary_file_frame("1.lf0", dimension=1)
-    bin_bap_2,_ = load_binary_file_frame("1.bap", dimension=1)
-    bin_mc_2,_ = load_binary_file_frame("1.mgc", dimension=60)
-    pass
+    bap = bap.astype(np.float32)
+    write_binfile(bap, os.path.join('./', file_id + '.bap'),dtype=np.float32)
     ### convert bapd to bap ###
 
-process("1.wav")
+def synthesis():
+    # pdb.set_trace()
+    lf0_file = "p225_001.lf0"
+    bap_file_name="p225_001.bap"
+    mgc_file_name="p225_001.mgc"
+    fl=4096
+    sr=48000
+    # pdb.set_trace()
+    lf0 = read_binfile(lf0_file, dim=1, dtype=np.float32)
+    zeros_index = np.where(lf0 == -1E+10)
+    nonzeros_index = np.where(lf0 != -1E+10)
+    f0 = lf0.copy()
+    f0[zeros_index] = 0
+    f0[nonzeros_index] = np.exp(lf0[nonzeros_index])
+    f0 = f0.astype(np.float64)
+    bap_dim = 5
+    bap = read_binfile(bap_file_name, dim=bap_dim, dtype=np.float32)
+    ap = pyworld.decode_aperiodicity(bap.astype(np.float64).reshape(-1, bap_dim), sr, fl)
+    mc = read_binfile(mgc_file_name, dim=60, dtype=np.float32)
+    alpha = pysptk.util.mcepalpha(sr)
+    sp = pysptk.mc2sp(mc.astype(np.float64), fftlen=fl, alpha=alpha)
+    wav = pyworld.synthesize(f0, sp, ap, sr, 5)
+    x2 = wav * 32768
+    x2 = x2.astype(np.int16)
+    scipy.io.wavfile.write("resynthesis.wav", sr, x2)
+    # os.chdir(cur_dir)
 
-read_binfile("lc.f0")
+process("p225_001.wav")
+synthesis()
+# read_binfile("lc.f0")
